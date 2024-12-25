d96f0969c0ac55e05571fc4a10f9addd3afbf9ff
Christian Patiño <christiandp88@gmail.com>
2024-12-25 17:16:04 -0300

    Se agrego el contenedor p para los emoticones de gatos y se modifico el main.js para que maneje los eventos de click en cada botón

--- src/main.js
+++ src/main.js
@@ -1,2 +1,34 @@
 import './style.css'
 
+let btnClick = document.querySelectorAll('button')
+let contenedor = document.querySelector('#contenedor')
+
+let gatosSeguidos1 = '😺😺😺😺😺😺'
+let gatosSeguidos2 = '😸😸😸😸😸😸'
+let gatosSeguidos3 = '😹😹😹😹😹😹'
+
+btnClick.forEach( (btnGato) => {
+    btnGato.addEventListener('click', () =>{
+        switch (btnGato.id) {
+            case 'btn-1':
+                contenedor.textContent += '😺'
+                if (contenedor.textContent.includes(gatosSeguidos1)) mantenerCajas()
+                break;
+            case 'btn-2':
+                contenedor.textContent += '😸'
+                if (contenedor.textContent.includes(gatosSeguidos2)) mantenerCajas()
+                break;
+            case 'btn-3':
+                contenedor.textContent += '😹'
+                if (contenedor.textContent.includes(gatosSeguidos3)) mantenerCajas()
+                break;
+            default:
+                break;
+        }
+    })
+})
+
+let mantenerCajas = () => {
+    contenedor.textContent = contenedor.textContent.substring(0, contenedor.textContent.lastIndexOf('◼️') + 1)
+    contenedor.textContent += '◼️'
+}
